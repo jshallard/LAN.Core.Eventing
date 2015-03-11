7535dec087055a29789fb885051f1ce572ad0aec
jshallard <jshallard@appriver.com>
2015-03-11 16:27:12 +0100

    Updating to pass querystring

--- LAN.Core.Eventing.SignalR/ISignalRGroupRegistrar.cs
+++ LAN.Core.Eventing.SignalR/ISignalRGroupRegistrar.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
 
@@ -6,13 +7,13 @@ namespace LAN.Core.Eventing.SignalR
 	[ContractClass(typeof(SignalRGroupRegistrarContract))]
 	public interface ISignalRGroupRegistrar
 	{
-		Task<string[]> GetGroupsForUser(string usersName);
+		Task<string[]> GetGroupsForUser(string usersName, Dictionary<string, string> queryStrings);
 	}
 
 	[ContractClassFor(typeof(ISignalRGroupRegistrar))]
 	abstract class SignalRGroupRegistrarContract : ISignalRGroupRegistrar
 	{
-		Task<string[]> ISignalRGroupRegistrar.GetGroupsForUser(string usersName)
+		Task<string[]> ISignalRGroupRegistrar.GetGroupsForUser(string usersName, Dictionary<string, string> toDictionary)
 		{
 			Contract.Requires(usersName != null);
 			Contract.Ensures(Contract.Result<Task<string[]>>() != null);
